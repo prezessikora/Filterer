427220fe2d449ef2d41c824390d515223392d8f1
Krzysztof Sikora <prezes.sikora@gmail.com>
2017-05-10 14:15:27 +0200

    Initial Commit

---
+++ Filterer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Filterer
+//
+//  Created by Krzysztof Sikora on 10/05/17.
+//  Copyright © 2017 Design Love. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
